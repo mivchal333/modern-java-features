1c928aa8c1b85a0d28ec8461ff00cb0c71fe4c5a
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-06 23:10:55 +0200

    Java 9: Set.of() method

---
+++ src/main/java/java9/setOf/ImageConverter.java
@@ -0,0 +1,18 @@
+package java9.setOf;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ImageConverter {
+    private final String JPG_FILE_KEY = "jpg";
+    private final String PNG_FILE_KEY = "png";
+    private final String BMP_FILE_KEY = "bmp";
+
+
+    public Set<String> getAvailableFileKeys() {
+        // implement here
+
+
+        return new TreeSet<>();
+    }
+}
