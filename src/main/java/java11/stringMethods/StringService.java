795650e20015a58a5e3a06332ddd66436ef7b2a3
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-07 23:27:26 +0200

    Java 11: new string methods

---
+++ src/main/java/java11/stringMethods/StringService.java
@@ -0,0 +1,22 @@
+package java11.stringMethods;
+
+import java.util.List;
+
+public class StringService {
+
+    List<String> getOnlyNotBlankStrings(List<String> input) {
+        // implement here
+
+        return input;
+    }
+
+    List<String> getStrippedTextLines(String text) {
+        // implement here
+        return null;
+    }
+
+    List<String> extendFoundStringByRepeatSomeTimes(List<String> list, String searchedText, int nTimesRepeat) {
+        // implement here
+        return list;
+    }
+}
