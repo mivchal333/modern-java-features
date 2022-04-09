586099f22b292bb12facd6786f5872367a583238
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-09 11:43:39 +0200

    Java 12: string indent

---
+++ src/main/java/java12/indent/TextBlockFormatter.java
@@ -0,0 +1,11 @@
+package java12.indent;
+
+public class TextBlockFormatter {
+    private final int CODE_TEXT_BLoCK_INDENT = 4;
+
+    String formatLinesToCodeBlockIndent(String code) {
+
+
+        return "";
+    }
+}
