435d0111bd97c9e2886e1cfbe0f493382700c426
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-09 12:24:14 +0200

    Java 12: transform update

--- src/main/java/java12/transform/PalindromeChecker.java
+++ src/main/java/java12/transform/PalindromeChecker.java
@@ -3,6 +3,8 @@ package java12.transform;
 public class PalindromeChecker {
 
     boolean isPalindrome(String text) {
+        // implement here
+        // TIP: you can use StringBuilder.reverse method
 
 
         return false;
