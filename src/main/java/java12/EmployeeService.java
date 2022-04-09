5b9a9ec4aeb83acb14668c05a1c4e584482c654c
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-09 11:10:41 +0200

    Java 12: teeing operator

---
+++ src/main/java/java12/EmployeeService.java
@@ -0,0 +1,12 @@
+package java12;
+
+import java.util.List;
+
+public class EmployeeService {
+
+    double getAvgEmployeeSalary(List<Employee> employees) {
+        // implement here
+
+        return 0.0;
+    }
+}
