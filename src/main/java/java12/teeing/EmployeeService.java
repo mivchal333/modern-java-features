54f14dca488fef3bce42bce54d723a9728d7d8f0
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-10 23:33:57 +0200

    Java 12: help comment

--- src/main/java/java12/teeing/EmployeeService.java
+++ src/main/java/java12/teeing/EmployeeService.java
@@ -6,6 +6,7 @@ public class EmployeeService {
 
     double getAvgEmployeeSalary(List<Employee> employees) {
         // implement here
+        // use List::stream method
 
         return 0.0;
     }
