8ec8a0e36ad929da7366fd4f63f270e1bb37e11e
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-10 23:32:45 +0200

    Java 12: indent update

--- src/main/java/java12/indent/NoIndentTextFormatter.java
+++ src/main/java/java12/indent/NoIndentTextFormatter.java
@@ -3,6 +3,8 @@ package java12.indent;
 public class NoIndentTextFormatter {
 
     String noIndentText(String text) {
+        // implement here
+        // remove indent
 
         return "";
     }
--- src/main/java/java12/indent/TextBlockFormatter.java
+++ src/main/java/java12/indent/TextBlockFormatter.java
@@ -4,8 +4,9 @@ public class TextBlockFormatter {
     private final int CODE_TEXT_BLoCK_INDENT = 4;
 
     String formatLinesToCodeBlockIndent(String code) {
+        // implement here
+        // use CODE_TEXT_BLoCK_INDENT as padding value
 
-
-        return code.indent(CODE_TEXT_BLoCK_INDENT);
+        return "";
     }
 }
