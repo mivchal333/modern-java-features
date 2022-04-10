7a3c69c745f9d3ea9a700673f93fefd738c4d732
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-10 23:24:12 +0200

    Java 11: help comments

--- src/main/java/java11/stringMethods/StringService.java
+++ src/main/java/java11/stringMethods/StringService.java
@@ -12,11 +12,13 @@ public class StringService {
 
     List<String> getStrippedTextLines(String text) {
         // implement here
+        // split text to lines and strip
         return null;
     }
 
     List<String> extendFoundStringByRepeatSomeTimes(List<String> list, String searchedText, int nTimesRepeat) {
         // implement here
+        // search for searchedText in a list. Change this string by repeat n times if found
         return list;
     }
 }
