5580ba007d16b5efa86ecf6209aac12db955ede4
Micha Krysiuk <Michal.Krysiuk@ttpsc.pl>
2022-04-21 21:21:49 +0200

    teeing operator tip

--- src/main/java/java12/teeing/event/ParticipationService.java
+++ src/main/java/java12/teeing/event/ParticipationService.java
@@ -53,6 +53,13 @@ public class ParticipationService {
 
     EventParticipation processEventGuests(List<Guest> guests) {
         // implement here
+        // return EventParticipation with sum of all participants and names of participants, who will attend
+
+        // TIP
+        // collector1: use Collectors.filtering for filtering ony attend guests,
+        // then collect theirs names to list (use Collectors.mapping)
+        // collector2: return sum of all participants
+
 
         return new EventParticipation(Collections.emptyList(), 0);
     }
